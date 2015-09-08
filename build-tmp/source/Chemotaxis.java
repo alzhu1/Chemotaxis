c4e4e514a8c36f13968ce66195bd097b52db2c08
unknown <alzhu1@697-334-Harlond.sf.k12.internal>
2015-09-08 08:18:56 -0700

    halfway done, must add a food component

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  //declare bacteria variables here   
  Bacteria [] colony;
- void setup()   
+ public void setup()   
  {
  	size(500,500);
  	//initialize bacteria variables here
@@ -10,7 +26,7 @@
  		colony[i] = new Bacteria();
  	}
  }   
- void draw()   
+ public void draw()   
  {
  	background(127);
  	noFill();
@@ -32,15 +48,24 @@
  		bacCol = (int)(Math.random()*256);
 
  	}
- 	void move()
+ 	public void move()
  	{
  		bacX = bacX + (int)(Math.random()*4-2);
  		bacY = bacY + (int)(Math.random()*4-2);
  	}
- 	void show()
+ 	public void show()
  	{
  		stroke(bacCol);
  		ellipse(bacX,bacY,10,10);
  	}
  	//lots of java!   
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
